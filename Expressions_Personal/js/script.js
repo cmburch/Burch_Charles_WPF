34bafe3ed230f7deb97c52f170f1cb74715ef030
cmburch <georgia_beast89@yahoo.com>
2013-08-15 01:21:03 -0600

    var gasTotalPrices assigned

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,4 +4,4 @@ alert("This page was designed to calculate gas mileage and calculate the total i
 var milesDriven = parseFloat(prompt("How many miles can you drive, before a refill ? Enter in whole number."));
 var gasConsumed = parseFloat(prompt("How many gallons of gas does your car hold full? Enter whole number."));
 var mpgTotal = milesDriven / gasConsumed;
-var gasTotalPrices 
+var gasTotalPrices = gasConsumed * total cost;
