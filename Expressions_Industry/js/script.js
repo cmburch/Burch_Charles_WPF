f27b3cbdb2109d9eab4bd719de675fdb757f6c7c
cmburch <georgia_beast89@yahoo.com>
2013-08-15 16:35:50 -0600

    alerts message was created to give user brief description on page purpose

---
+++ Expressions_Industry/js/script.js
@@ -0,0 +1,5 @@
+// JavaScript
+
+
+//alerts user brief description on page purpose 
+alert(" This page was designed to help web developers make a good choice of comparing 2 computers price per gigabyte size.");
