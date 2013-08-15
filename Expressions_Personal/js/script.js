34cb45c5d94b44bb53300b1fc4e1d27af391eeea
cmburch <georgia_beast89@yahoo.com>
2013-08-15 01:06:02 -0600

    var mpgTotal declared

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,3 +3,4 @@
 alert("This page was designed to calculate gas mileage and calculate the total it takes to fill up your tank. ");
 var milesDriven = parseFloat(prompt("How many miles can you drive, before a refill ? Enter in whole number."));
 var gasConsumed = parseFloat(prompt("How many gallons of gas does your car hold full? Enter whole number."));
+var mpgTotal
