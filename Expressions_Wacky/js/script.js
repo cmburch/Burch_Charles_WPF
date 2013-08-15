13d0ec177c6a6cab28cc25c6d429183ebdf56861
cmburch <georgia_beast89@yahoo.com>
2013-08-15 04:05:28 -0600

    alert (welcome) added

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,4 +1,5 @@
 // JavaScript
 
-var welcome = "Welcome /! To Burch’s Donuts ordering page. We take orders by the dozen";
+var welcome = "Welcome to Burch’s Donuts ordering page. We take orders by the dozen.";
 var priceDetails = "Each dozen of donuts cost /$ 12";
+alert (welcome);
