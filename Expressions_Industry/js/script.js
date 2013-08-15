8ccfb619c9f40503341162b4d1b24754d1f2325d
cmburch <georgia_beast89@yahoo.com>
2013-08-15 16:37:54 -0600

    var priceComp declared and assigned

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,3 +3,5 @@
 
 //alerts user brief description on page purpose 
 alert(" This page was designed to help web developers make a good choice of comparing 2 computers price per gigabyte size.");
+// ask user for input of computer 1 price
+var priceComp = parseFloat(prompt("please enter the price of the first computer.",600));
