4a3b343f03a7194c6dc7edecd442e9f23b326321
cmburch <georgia_beast89@yahoo.com>
2013-08-15 00:55:29 -0600

    var milesDriven declared and assigned

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,4 @@
 // JavaScript
 
 alert("This page was designed to calculate gas mileage and calculate the total it takes to fill up your tank. ");
+var milesDriven = parseFloat(prompt("How many miles can you drive, before a refill ? Enter in whole number.")); 
