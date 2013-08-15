6bdf20f6f1973ff42cebe5abb0522ba7a9f77c75
cmburch <georgia_beast89@yahoo.com>
2013-08-15 16:44:13 -0600

    var gigabyteSize2 declared and assigned

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -9,3 +9,5 @@ var priceComp = parseFloat(prompt("please enter the price of the first computer.
 var gigabyteSize = parseInt(prompt("please enter the gigabyte size of the first computer",50));
 // ask user for input of computer 2 price
 var priceComp2 = parseFloat(prompt("please enter the price of the second computer.",700));
+// ask user for input of computer 2 gigabyte size
+var gigabyteSize2 = parseInt(prompt("please enter the gigabyte size of the second computer",60));
