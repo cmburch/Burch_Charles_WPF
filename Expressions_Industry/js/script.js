5c64f85883b0b5d2e2af2cc3ec44ad7750328290
cmburch <georgia_beast89@yahoo.com>
2013-08-15 17:12:11 -0600

    alert message addded

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,13 +4,13 @@
 //alerts user brief description on page purpose 
 alert(" This page was designed to help web developers make a good choice of comparing 2 computers price per gigabyte size.");
 // ask user for input of computer 1 price
-var priceComp = parseFloat(prompt("please enter the price of the first computer.",600));
+var priceComp = parseFloat(prompt("please enter the price of the first computer.", 600));
 // ask user for input of computer 1 gigabyte size
 var gigabyteSize = parseInt(prompt("please enter the gigabyte size of the first computer",50));
 // ask user for input of computer 2 price
 var priceComp2 = parseFloat(prompt("please enter the price of the second computer.",700));
 // ask user for input of computer 2 gigabyte size
-var gigabyteSize2 = parseInt(prompt("please enter the gigabyte size of the second computer",60));
+var gigabyteSize2 = parseInt(prompt("please enter the gigabyte size of the second computer " ,60));
 //collects user input and divides computer1 price by the gigabyte 
 var pricePerGigibyte1 = priceComp / gigabyteSize;
 //collects user input and divides computer2 price by the gigabyte2
@@ -30,4 +30,6 @@ var gigabytesz102 = PerGigibyte2 * gbVariety[1];
 //calculating price per gigabytes computer2
 var gigabytesz502 = PerGigibyte2 * gbVariety[2];
 //displays total of price per gigabyte for computer 1 and computer 2
-alert(" Below are the quick price references for 5,10,50 gigabytes ");
+alert(" Below are the quick price references for 5,10,50 gigabytes ")
+
+“Every 10 gigabytes the price is:” + “$” + gigabytesz10 + “</br>”
