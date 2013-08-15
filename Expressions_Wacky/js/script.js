1424ef89d5e99147c8fc7dace686c7f4d259f4b7
cmburch <georgia_beast89@yahoo.com>
2013-08-15 04:09:21 -0600

    var donutOrder declared and assigned

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,3 +5,4 @@ var priceDetails = "Each dozen of donuts cost $ 12.";
 alert (welcome);
 alert (priceDetails);
 var pricePerDozen=12;
+var donutOrder = parseInt(prompt("How many fresh baked donuts would you like to enjoy? Enter in a whole number.We are only taking orders by the dozen."));
