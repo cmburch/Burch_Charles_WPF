4e1446608069934216221a3ec95d73d408134c6d
cmburch <georgia_beast89@yahoo.com>
2013-08-15 18:01:33 -0600

    //alerts user brief description on page purpose comment added

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,5 +1,5 @@
 // JavaScript
-
+//alerts user brief description on page purpose 
 alert("This page was designed to calculate gas mileage and calculate the total it takes to fill up your tank. ");
 var milesDriven = parseFloat(prompt("How many miles can you drive, before a refill ? Enter in whole number." , 200));
 var gasConsumed = parseFloat(prompt("How many gallons of gas does your car hold full? Enter whole number." , 18));
