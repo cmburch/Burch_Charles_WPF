d3835bef15714e558a38ee52be4a06c78fb31a70
cmburch <georgia_beast89@yahoo.com>
2013-08-15 20:58:10 -0600

    //Slice of Pie part 1 added

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -7,3 +7,12 @@ var humanYears = 3;
 var dogYears = humanYears * 7;
 //display message to user sparky human and dog years
 alert("sparky is " + humanYears + " human years old which is " + dogYears + " in dog years.");
+
+
+//Slice of Pie part 1
+var people=20;
+var slices=10;
+var  pizzas=10;
+var slicesTotal=slices * pizzas;
+var slicesEach = slicesTotal / people;
+console.log("Each person ate " + slices Each +  "slices of pizza at the party.") ;
