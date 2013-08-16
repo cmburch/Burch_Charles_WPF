a38181cfd275b0fe7686edef2383cb1bba4c55ad
cmburch <georgia_beast89@yahoo.com>
2013-08-15 18:56:49 -0600

    alert("Your cars miles per gallon is " + mpgTotal + " gallons and your total price to fill your car is " + gasTotalPrices + "dollars" );changed

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -6,9 +6,9 @@ alert(" This page was designed to help web developers make a good choice of comp
 // ask user for input of computer 1 price
 var priceComp = parseFloat(prompt("please enter the price of the first computer.", 600));
 // ask user for input of computer 1 gigabyte size
-var gigabyteSize = parseInt(prompt("please enter the gigabyte size of the first computer",50));
+var gigabyteSize = parseInt(prompt("please enter the gigabyte size of the first computer" ,50));
 // ask user for input of computer 2 price
-var priceComp2 = parseFloat(prompt("please enter the price of the second computer.",700));
+var priceComp2 = parseFloat(prompt("please enter the price of the second computer." ,700));
 // ask user for input of computer 2 gigabyte size
 var gigabyteSize2 = parseInt(prompt("please enter the gigabyte size of the second computer " ,60));
 //collects user input and divides computer1 price by the gigabyte 
@@ -16,7 +16,7 @@ var pricePerGigibyte1 = priceComp / gigabyteSize;
 //collects user input and divides computer2 price by the gigabyte2
 var pricePerGigibyte2 = priceComp2 / gigabyteSize2;
 //inputs for calculating price per gigabytes for computer 1 and computer2
-var gbVariety = new array(5,10,50);
+var gbVariety = [5,10,50];
 // calculating price per gigabytes computer 1
 var gigabytesz5 = PerGigibyte1 * gbVariety[0];
 //calculating price per gigabytes computer 1
@@ -30,12 +30,14 @@ var gigabytesz102 = PerGigibyte2 * gbVariety[1];
 //calculating price per gigabytes computer2
 var gigabytesz502 = PerGigibyte2 * gbVariety[2];
 //displays total of price per gigabyte for computer 1 and computer 2
-alert(" Below are the quick price references for 5,10,50 gigabytes ")
+alert(" Below are the quick price references for 5,10,50 gigabytes " + "</br>" +
 
-“Every 10 gigabytes the price is:” + “$” + gigabytesz10 + “</br>”
-“Every 50gigabytes the price is:” + “$” + gigabytesz50 + “</br>”
-“Computer 2” + “</br>”
-“Every 5 gigabytes the price is:” + “$” + gigabytesz52 + “</br>”
-“Every 10 gigabytes the price is:” + “$” + gigabytesz102 + “</br>”
-“Every 50gigabytes the price is:” + “$” + gigabytesz502 + “</br>”
-“Choose the deal that best fits you.”);
+"Computer 1" + "</br>" +
+"Every 5 gigabytes the price is:" + "$" + gigabytesz5 + "</br>" +
+"Every 10 gigabytes the price is:" + "$" + gigabytesz10 + "</br>" +
+"Every 50gigabytes the price is:" + "$" + gigabytesz50 + "</br>" +
+"Computer 2" + "</br>" +
+"Every 5 gigabytes the price is:" + "$" + gigabytesz52 + "</br>" +
+"Every 10 gigabytes the price is:" + "$" + gigabytesz102 + "</br>" +
+"Every 50gigabytes the price is:" + "$" + gigabytesz502 + "</br>" +
+"Choose the deal that best fits you.");
