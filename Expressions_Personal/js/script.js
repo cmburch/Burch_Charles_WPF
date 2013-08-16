77977fd5941a6466c8eb4f28899cf1997aebfc9e
cmburch <georgia_beast89@yahoo.com>
2013-08-15 18:03:52 -0600

    // ask user for input of gallons of gas their car can hold comment added

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,7 +3,9 @@
 alert("This page was designed to calculate gas mileage and calculate the total it takes to fill up your tank. ");
 // ask user for input of miles driven
 var milesDriven = parseFloat(prompt("How many miles can you drive, before a refill ? Enter in whole number." , 200));
+// ask user for input of gallons of gas their car can hold
 var gasConsumed = parseFloat(prompt("How many gallons of gas does your car hold full? Enter whole number." , 18));
+
 var mpgTotal = milesDriven / gasConsumed;
 var gasTotalPrices = gasConsumed * mpgTotal;
 alert("Your cars miles per gallon is " + mpgTotal + " gallons and your total price to fill your car is " + gasTotalPrices + " dollars per gallon. ");
