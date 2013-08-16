a7a9f119ad303afad755a1e011fdbfce1e34cc65
cmburch <georgia_beast89@yahoo.com>
2013-08-15 18:29:38 -0600

    alert("Your cars miles per gallon is " + mpgTotal + " gallons and your total price to fill your car is " + gasTotalPrices + "dollars" );modified

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,11 +5,12 @@ alert("This page was designed to calculate gas mileage and calculate the total i
 var milesDriven = parseFloat(prompt("How many miles can you drive, before a refill ? Enter in whole number." , 200));
 // ask user for input of gallons of gas their car can hold
 var gasConsumed = parseFloat(prompt("How many gallons of gas does your car hold full? Enter whole number." , 18));
-//calculates the miles per gallon total
+//calculates price per gallon
 var gasPricePerGallon = parseFloat(prompt("How much do you pay for a gallon of gas?.", 2.75));
+//calculates the miles per gallon total
 var mpgTotal = milesDriven / gasConsumed;
 //calculates the gas price total
-var gasTotalPrices = gasConsumed * mpgTotal;
+var gasTotalPrices = gasPricePerGallon * mpgTotal;
 //alerts user brief description which gives total
-alert("Your cars miles per gallon is " + mpgTotal + " gallons and your total price to fill your car is " + gasTotalPrices + " dollars per gallon. ");
+alert("Your cars miles per gallon is " + mpgTotal + " gallons and your total price to fill your car is " + gasTotalPrices + "dollars" );
 
